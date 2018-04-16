ac148c969df85b1ef66309aa14a4470448c0ec0b
Moisi Xhaferaj <elektroxray@gmail.com>
2018-04-16 18:21:56 +0000

    Update AT85sensorquery.h

--- AT85sensorquery.h
+++ AT85sensorquery.h
@@ -5,7 +5,7 @@ AT85sensorquery Library for I2C sensors, based on ATTINY-85, querying functions
 These functions have the purpose of reading through the ATTINY-85 library the 10 bit output pin of the Pin2 analogread of the attiny85 connected through the I2C bus to the Arduino Uno master.
 
 Created by Moisi Xhaferaj, 05/03/2018.
-Released into the public domain.
+
 
 */
 
